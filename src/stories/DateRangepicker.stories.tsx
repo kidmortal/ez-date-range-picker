962ecc83b5548285caaae8e3bd3fa4e7a558b9cf
Kidmortal <kidmortal@gmail.com>
2021-11-03 15:25:01 -0300

    sla

--- src/stories/DateRangepicker.stories.tsx
+++ src/stories/DateRangepicker.stories.tsx
@@ -1,20 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { DateRangePicker } from '../DateRangePicker';
-
-const Container = styled.div``;
-
-const View = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  background-color: lightgray;
-`;
 
 export default {
   title: 'Kidmortal/Datepicker',
